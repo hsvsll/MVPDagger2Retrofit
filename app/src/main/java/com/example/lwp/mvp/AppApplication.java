bb124923f324cb2610770827411359c6bbd85989
hsvsll <wdyj0.0@foxmail.com>
2016-08-24 16:52:05 +0800

    mvp、 dagger2、retrofit  test

---
+++ app/src/main/java/com/example/lwp/mvp/AppApplication.java
@@ -0,0 +1,33 @@
+package com.example.lwp.mvp;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.example.lwp.mvp.data.api.ApiServiceModule;
+
+/**
+ * Created by clevo on 2015/6/9.
+ */
+public class AppApplication  extends Application{
+
+    private AppComponent appComponent;
+
+
+    public static AppApplication get(Context context){
+        return (AppApplication)context.getApplicationContext();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        appComponent=DaggerAppComponent.builder()
+                .appModule(new AppModule(this))
+                .apiServiceModule(new ApiServiceModule())
+                .build();
+    }
+
+
+    public AppComponent getAppComponent() {
+        return appComponent;
+    }
+}
